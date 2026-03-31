7c5209d59ad3b9c865d28de44bc725502104bedd
Andrey Akulov <andreyjamer@gmail.com>
2026-03-31 17:30:52 +0500

    XPR-178: Updated react native example

--- examples/react-native/webSdk.ts
+++ examples/react-native/webSdk.ts
@@ -10,8 +10,8 @@ class ProtonSDK {
 
   constructor() {
     this.chainId =
-      '71ee83bcf52142d61019d95f9cc5427ba6a0d7ff8accd9e2088ae2abeaf3d3dd';
-    this.endpoints = ['https://testnet.protonchain.com']; // Multiple for fault tolerance
+      '384da888112027f0321850a169f737c33e53b388aad48b5adace4bab97f437e0';
+    this.endpoints = ['https://proton.greymass.com']; // Multiple for fault tolerance
     this.requestAccount = 'taskly'; // optional
     this.session = null;
     this.link = null;
